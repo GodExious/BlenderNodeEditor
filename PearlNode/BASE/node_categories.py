2fdd54f07f42a48da895ac65d8d428e5b7e84866
SoTosorrow <50233205+SoTosorrow@users.noreply.github.com>
2021-09-12 17:33:03 +0800

    node_base-1

--- PearlNode/BASE/node_categories.py
+++ PearlNode/BASE/node_categories.py
@@ -1,16 +1,17 @@
 import nodeitems_utils
 
-class NodeCategory(nodeitems_utils.NodeCategory):
+class PearlNodeCategory(nodeitems_utils.NodeCategory):
     @classmethod
     def poll(cls, context):
-        return context.space_data.tree_type in {'NodeTree','NodeTreeGroup'}
+        return context.space_data.tree_type in {'PearlNodeTree','PearlNodeTreeGroup'}
 
 node_categories = [
-    NodeCategory('INPUT',"Input",items=[
-        nodeitems_utils.NodeItem('Test'),
+    PearlNodeCategory('INPUT',"Input",items=[
+        nodeitems_utils.NodeItem('NodeFloatInput'),
+        nodeitems_utils.NodeItem('NodeStringInput'),
     ]),
-    NodeCategory('OUTPUT',"Output",items=[
-        nodeitems_utils.NodeItem('Test'),
+    PearlNodeCategory('OUTPUT',"Output",items=[
+        nodeitems_utils.NodeItem('NodeResult'),
     ]),
 
 ]
@@ -19,11 +20,11 @@ node_categories = [
 
 def register():
     try:
-        nodeitems_utils.unregister_node_categories("NodeCategory")
+        nodeitems_utils.unregister_node_categories("PearlNodeCategory")
     except Exception:
         pass
-    nodeitems_utils.register_node_categories("NodeCategory", node_categories)
+    nodeitems_utils.register_node_categories("PearlNodeCategory", node_categories)
 
 
 def unregister():
-    nodeitems_utils.unregister_node_categories("NodeCategory")
+    nodeitems_utils.unregister_node_categories("PearlNodeCategory")
---
+++ PearlNode/Output/node_output.py
@@ -0,0 +1,63 @@
+import bpy
+from bpy.props import *
+from ..BASE.node_base import NodeBase
+from mathutils import Vector
+
+
+def set_active_output(self, context):
+    if self.set_as_output:
+        for node in self.id_data.nodes:
+            if node.bl_idname == self.bl_idname and node != self:
+                node.set_as_output = False
+
+        # set active output
+        bpy.context.window_manager.sp_viewer_node = self.name
+        bpy.context.scene.sp_viewer_tree = self.id_data
+        self.execute_tree()
+
+
+class NodeResult(NodeBase):
+    """A simple Result node"""
+    bl_idname = "NodeResult"
+    bl_label = 'Node Result'
+
+    default_value: StringProperty(default='Need Update', name='Result Value')
+    precision: IntProperty(name='Precision', default=3, update=set_active_output)
+    # set active and update
+    set_as_output: BoolProperty(default=False,
+                                update=set_active_output,
+                                description='Set as active output')
+
+    def init(self, context):
+        self.create_input('PearlNodeSocketResult', 'input', 'Input')
+        self.width = 200
+
+    def draw_buttons(self, context, layout):
+        col = layout.column()
+        col.prop(self, 'set_as_output', text='Active Output')
+
+        col.prop(self, 'precision', icon='DOT', emboss=False)
+        box = col.box().column(align=1)
+        box.prop(self, 'default_value', text='')
+
+    def process(self, context, id, path):
+        ip = self.inputs[0].get_value()
+        if isinstance(ip, float):
+            self.default_value = f'{round(ip, self.precision)}'
+        elif isinstance(ip, Vector):
+            d_val = [round(num, self.precision) for num in list(ip)]
+            self.default_value = F'{tuple(d_val)}'
+        else:
+            self.default_value = f'{ip}'
+
+
+def register():
+    bpy.utils.register_class(NodeResult)
+    bpy.types.WindowManager.sp_viewer_node = StringProperty(name='Viewer output name')
+    bpy.types.Scene.sp_viewer_tree = PointerProperty(name='Viewer output tree', type=bpy.types.NodeTree)
+
+
+def unregister():
+    bpy.utils.unregister_class(NodeResult)
+    del bpy.types.WindowManager.sp_viewer_node
+    del bpy.types.Scene.sp_viewer_tree
